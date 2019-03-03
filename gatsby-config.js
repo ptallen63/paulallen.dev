831b1cab3f1ade1dc88f25ae29d609fee9c20897
Paul Allen <ptallen63@gmail.com>
2019-03-03 13:43:54 -0500

    init commit

---
+++ gatsby-config.js
@@ -0,0 +1,52 @@
+module.exports = {
+  siteMetadata: {
+    title: `paulallen.dev`,
+    description: `Paul Allen Dev Site`,
+    author: `@ptallen63`,
+  },
+  plugins: [
+    `gatsby-plugin-react-helmet`,
+    `gatsby-plugin-sass`,
+    `gatsby-plugin-netlify`,
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        name: `images`,
+        path: `${__dirname}/src/images`,
+      },
+    },
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        path: `${__dirname}/src/pages/projects`,
+        name: `projects`
+      }
+    },
+    {
+      resolve: `gatsby-plugin-sitemap`
+    },
+    {
+      resolve: `gatsby-plugin-google-tagmanager`,
+      options: {
+        id: "GTM-MVMLPCZ"
+      }
+    },
+    `gatsby-transformer-sharp`,
+    `gatsby-plugin-sharp`,
+    {
+      resolve: `gatsby-plugin-manifest`,
+      options: {
+        name: `gatsby-starter-default`,
+        short_name: `starter`,
+        start_url: `/`,
+        background_color: `#663399`,
+        theme_color: `#663399`,
+        display: `minimal-ui`,
+        icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
+      },
+    },
+    // this (optional) plugin enables Progressive Web App + Offline functionality
+    // To learn more, visit: https://gatsby.dev/offline
+    // 'gatsby-plugin-offline',
+  ],
+}
---
+++ src/pages/proejcts/111111-test/index.md
@@ -0,0 +1,23 @@
+---
+title: 'Grade Calculate Mobile App'
+path: '/project/gradecalculate-mobile'
+dateCompleted: '2015-05-26'
+status: 'Active'
+shortDescription: 'Mobile App for Grade Calculate'
+tags: ['Javascript', 'Ionic', 'iOS']
+images: [
+      'https://s3.amazonaws.com/gradecalculate/images/IMG_1233.PNG',
+      'https://s3.amazonaws.com/gradecalculate/images/IMG_1235.PNG',
+      'https://s3.amazonaws.com/gradecalculate/images/IMG_1234.PNG',
+    ]
+
+url: 'https://itunes.apple.com/us/app/grade-calculate/id1121430103?mt=8'
+type: 'Mobile App'
+cover: ''
+frontImage: 'https://s3-us-west-2.amazonaws.com/paultallen.com/Images/GC+Mobile+App+Cover.png'
+show: true
+---
+
+## Description
+
+The Grade Calculate mobile app gives you the power of GradeCalculate.com on your mobile device. This was my first step into mobile development, and it has been a very successful experience. A new version of the mobile app is currently in development to bring the same look and feel of GradeCalculate 3.0 to the mobile App. This app was built with the Ionic framework which utilizes Angular 1.x. The next version will use React Native.
