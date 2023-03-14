bffed22f28ab3d69e4444ab298a991a0fff07c3e
Paul Allen <paul.allen@dashlane.com>
2023-03-14 08:59:55 -0400

    fix: remove text from front page typed comp

--- src/data/homepageTypedText.js
+++ src/data/homepageTypedText.js
@@ -1,11 +1,12 @@
 export default [
   "<span class='black'>Engineer</span>",
+  "<span class='black'>Leader</span>",
   "<span class='black'>Developer</span>",
   "<span class='black'>Consultant</span>",
+  "<span class='black'>Strategist</span>",
   "<span class='black'>Cool Guy</span>",
   "<span class='black'>Technologist</span>",
   "<span class='black'>Lover of bacon</span>",
-  "<span class='black'>Email enthusiast</span>",
   "<span class='black'>I&apos;ve done a lot of interesting things</span>",
   "<a class='black' href='projects'>Check some of them out <span class='grey'>>></span></a>",
 ];
